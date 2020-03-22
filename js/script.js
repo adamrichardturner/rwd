736cfb2e2fef012e4c3c0ab752771262bd07dc4a
adamrichardturner <adamrichardturner@hotmail.com>
2020-03-22 10:35:10 +0000

    updates

--- js/script.js
+++ js/script.js
@@ -1,15 +1,4 @@
 /* jshint esversion:6 */
-var myVar;
-
-function myFunction() {
-  myVar = setTimeout(showPage, 3000);
-}
-
-function showPage() {
-  document.getElementById("loader").style.display = "none";
-  document.getElementById("myDiv").style.display = "block";
-}
-
 $(document).ready(function() {
   $('#about').waypoint(function() {
     $('#man').addClass('animated fadeInRight');
@@ -35,13 +24,4 @@ $(window).scroll(function() {
   } else {
     $('.nav').removeClass('affix');
   }
-});
-
-$(document).ready(function() {
-  setTimeout(function() {
-    $("#cookieConsent").fadeIn(0.5);
-  }, 100);
-  $("#closeCookieConsent, .cookieConsentOK").click(function() {
-    $("#cookieConsent").fadeOut(200);
-  });
 });
